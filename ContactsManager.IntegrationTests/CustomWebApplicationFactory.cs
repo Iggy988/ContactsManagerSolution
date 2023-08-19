2179229108c4c7a53ee9221c7b80d7d9999f8208
Iggy988 <igor.rodic1988@gmail.com>
2023-08-19 23:23:00 +0200

    Implementing Tests in Clean Architecture project

---
+++ ContactsManager.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Enttities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRUDTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.UseEnvironment("Test");
+
+        builder.ConfigureServices(services => {
+            var descripter = services.SingleOrDefault(temp => temp.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descripter != null)
+            {
+                services.Remove(descripter);
+            }
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("DatbaseForTesting");
+            });
+        });
+    }
+}
+
